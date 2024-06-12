2e07f55c61a717f6745bad15437e39cdef168375
ayushsingh9919 <ayush.singh@maximl.com>
2024-06-12 11:32:09 +0530

    Frontend Setup

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  
+
+  return (
+    <>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem, odit tenetur sunt ipsam, excepturi assumenda corrupti laborum veniam consectetur qui, repudiandae illo amet tempore similique voluptatem? Praesentium delectus molestias, adipisci iure temporibus ratione maxime fuga possimus consequuntur a ipsa facilis architecto magnam accusamus necessitatibus accusantium dicta aperiam esse nesciunt deleniti.
+      <Button >Test Button</Button>
+    </>
+  )
+}
+
+export default App
